36e78dd92366d63ae167e4d9c0b5c2f22c822342
Onur Kaya <onurkaya8@outlook.com>
2020-02-15 22:33:40 +0300

    password reset completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
 
     before_save :downcase_email
 
@@ -50,6 +50,19 @@ class User < ApplicationRecord
         digest = send("#{attribute}_digest")
         return false if digest.nil?
         BCrypt::Password.new(digest).is_password?(token)
+    end
+
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    end
+
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
     end
 
     private
